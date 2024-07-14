3f08cbcb4e1e169ce96f4d93fe81ad3f80e1383d
Harsh Bharti <harshjnv2018@gmail.com>
2024-07-15 01:30:52 +0530

    first commit

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
